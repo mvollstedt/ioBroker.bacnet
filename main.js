a5284faa0b84267fa47bf9a2604d519c445bc8e6
mvollstedt <101109523+mvollstedt@users.noreply.github.com>
2025-07-02 16:47:13 +0200

    new6

--- main.js
+++ main.js
@@ -5,18 +5,18 @@ class Bacnet extends utils.Adapter {
         super({ ...options, name: 'bacnet' });
     }
     async onReady() {
-        this.log.info('BACnet Adapter started.');
+        this.log.info('BACnet Adapter gestartet.');
         const devices = this.config.devices || this.native.devices || [];
         for (const dev of devices) {
             const id = `device_${dev.address.replace(/\W/g, '_')}_${dev.objectType}_${dev.instance}_${dev.propertyId}`;
             await this.setObjectNotExistsAsync(id, {
                 type: 'state',
-                common:{name: dev.name||id, type:'mixed', role:'value', read:true, write:false},
+                common: { name: dev.name || id, type: 'mixed', role: 'value', read: true, write: false },
                 native: dev
             });
             this.log.info(`Created device: ${id}`);
         }
     }
 }
-if (require.main===module) new Bacnet();
+if (require.main === module) new Bacnet();
 else module.exports = options => new Bacnet(options);
