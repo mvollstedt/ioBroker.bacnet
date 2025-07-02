2eb69129452f727a3de9b7a4574c1a5d4d9ec12d
mvollstedt <101109523+mvollstedt@users.noreply.github.com>
2025-07-02 21:24:15 +0200

    new12

--- admin/tab.js
+++ admin/tab.js
@@ -1,6 +1,5 @@
 // global load, save, setChanged, values2table, table2values
 
-// 1) Fügt eine Zeile hinzu und signalisiert ioBroker die Änderung
 function addRow(onChange) {
   const tbody = document.querySelector('#devices tbody');
   const tr = document.createElement('tr');
@@ -16,23 +15,18 @@ function addRow(onChange) {
   tr.querySelectorAll('input').forEach(i => i.onchange = () => onChange(true));
 }
 
-// 2) Wird von adapter.js beim Laden aufgerufen
 function load(settings, onChange) {
   console.log('BACnet load', settings);
   values2table('devices', settings.devices || [], onChange);
   onChange(false);
 }
 
-// 3) Wird von adapter.js beim Klick auf Save aufgerufen
 function save(callback) {
   const devices = table2values('devices');
   console.log('BACnet save', devices);
   callback({ devices });
 }
 
-// 4) Expose für adapter.js
 window.load = load;
 window.save = save;
-
-// 5) Hook auf den +Button, damit er aktiv wird
 document.getElementById('add').onclick = () => addRow(setChanged);
